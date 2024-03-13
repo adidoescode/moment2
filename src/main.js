92ce17090c07f2bcda7f8f0607f63126a39e36eb
Adam Sjögren <adidoescode@gmail.com>
2024-03-13 18:14:00 +0100

    Gjort ändringar för att API:n ska hämtas.

--- src/main.js
+++ src/main.js
@@ -4,4 +4,36 @@
 
 // DOM 
 
+
+// Starta javascripten när hemsidan laddas in
+
+async function init() {
+    try {
+        const schedule = await getSchedule();
+        handleSchedule(schedule);
+    }
+    catch (error) {
+        console.error('Error with initializing:', error);
+    }
+}
 window.onload = init;
+
+async function getSchedule() {
+    try {
+        const response = await fetch('ramschema_ht23.json');
+        const data = await response.json();
+        return data;
+    }
+    catch (error) {
+        console.error('Error with fetch:', error);
+        throw error;
+    }
+}
+
+async function handleSchedule(schedule) {
+    console.log(schedule); //Test
+
+    schedule.forEach(item => {
+        console.log(item);
+    });
+}
